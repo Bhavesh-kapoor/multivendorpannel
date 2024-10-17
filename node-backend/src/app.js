3b2c2a295948d4d1463c40585ee9975f4edeb85f
Bhavesh Kapoor <bhaveshkapoor09@gmail.com>
2024-10-17 15:59:03 +0530

    feat: dashboard setup

--- node-backend/src/app.js
+++ node-backend/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import authenticationRoute from './routes/auth.route.js';
+import { index } from './controllers/admin/dashboard.controller.js';
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
@@ -11,7 +12,7 @@ app.use(express.static("public"));
 
 // AUTH ROUTES 
 app.use('/auth',authenticationRoute);
-
+app.get('/admin/dashboard',index)
 
 
 
