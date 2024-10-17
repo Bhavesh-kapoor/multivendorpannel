295614494dec9a87b5105079c549d86aa9c958ca
Bhavesh Kapoor <bhaveshkapoor09@gmail.com>
2024-10-17 12:48:19 +0530

    feat:  Mysql Connection with Json Response Setup

--- node-backend/src/index.js
+++ node-backend/src/index.js
@@ -1,12 +1,15 @@
 import app from "./app.js";
 import env from 'dotenv';
 env.config();
-
+import connectToDatabase from "./db/connection.js";
 
 try {
-    app.listen(process.env.PORT, () => {
-        console.log(`server is running at ${process.env.PORT}`);
+    await connectToDatabase().then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`server is running at ${process.env.PORT}`);
+        })
     })
+
 } catch (err) {
     console.log(err);
 }
