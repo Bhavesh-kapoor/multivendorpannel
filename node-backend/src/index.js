864788d9501b5faf4055cf838b5426e5d3023d02
Anand Stephan <anandstephan98@gmail.com>
2024-10-17 11:59:28 +0530

    feat:setup of styling

--- node-backend/src/index.js
+++ node-backend/src/index.js
@@ -1,13 +1,31 @@
 import app from "./app.js";
-import env from 'dotenv';
+import express from "express";
+import env from "dotenv";
+
+import { engine } from "express-handlebars";
 env.config();
 
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs", // Set file extension to '.hbs'
+    defaultLayout: "main", // Optional: layout file (like 'main.hbs')
+    layoutsDir: "./views/layouts", // Directory for layout files
+    partialsDir: "./views/partials", // Directory for partials
+  })
+);
+app.set("view engine", "hbs"); // Set view engine to 'hbs'
+app.set("views", "./views");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 try {
-    app.listen(process.env.PORT, () => {
-        console.log(`server is running at ${process.env.PORT}`);
-    })
+  app.listen(process.env.PORT, () => {
+    console.log(`server is running at ${process.env.PORT}`);
+  });
 } catch (err) {
-    console.log(err);
+  console.log(err);
 }
-
