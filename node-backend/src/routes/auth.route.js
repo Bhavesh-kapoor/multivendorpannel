3ad0f6eea8449bdd2f512b52fc37769e88fb0ed5
adarshsrivastav375 <adarshsrivastav.375@gmail.com>
2024-10-26 11:46:06 +0530

    feat: made authentication routes login and helper function for creating tokens

--- node-backend/src/routes/auth.route.js
+++ node-backend/src/routes/auth.route.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import {
   login,
-  loginHandler
 } from "../controllers/admin/Auth/auth.controller.js";
-let authenticationRoute = Router();
+let authRoute = Router();
 
-authenticationRoute.get("/login", login);
-authenticationRoute.post("/login", loginHandler);
+authRoute.post("/login", login);
+// authenticationRoute.post("/login", loginHandler);
 
-export default authenticationRoute;
+export default authRoute;
