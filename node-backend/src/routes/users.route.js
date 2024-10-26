e630b2193255e461ccdc2b3ffe761a9355db1d28
NabilaSophia <nabilasophia8@gmail.com>
2024-10-26 12:27:48 +0530

    feat:adding rating api

--- node-backend/src/routes/users.route.js
+++ node-backend/src/routes/users.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { index, create ,logout,rating} from "../controllers/admin/Users/user.controller.js";
+import verifyJwtToken from "../middleware/auth.middleware.js";
 const usersRoute = Router();
 
 usersRoute.get("/all", index);
 usersRoute.get("/add", create);
 usersRoute.get("/logout", logout);
-usersRoute.get("/rating",rating)
+usersRoute.get("/rating",verifyJwtToken, rating)
 export default usersRoute;
