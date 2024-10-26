e2ea39a6e00f9d0ce659758b01ce7e57b5a6d341
adarshsrivastav375 <adarshsrivastav.375@gmail.com>
2024-10-26 10:14:46 +0530

    vendors routes merged

--- node-backend/src/routes/integrated.route.js
+++ node-backend/src/routes/integrated.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import categoryRoute from "./category.route.js";
-import productRoutes from "./product.route.js"
+import productRoutes from "./product.route.js";
+import vendorRoutes from "./vendor.route.js"
 
 import subCategoryRoutes from './sub-category.route.js';
 const route = Router();
@@ -8,6 +9,7 @@ const route = Router();
 route.use("/categories", categoryRoute);
 route.use("/subcategories", subCategoryRoutes);
 route.use("/products", productRoutes);
+route.use("/vendors", vendorRoutes);
 
 
 
