dde2d75a3acd5470817dd5fe519aeeb19f5e1099
adarshsrivastav375 <adarshsrivastav.375@gmail.com>
2024-10-28 13:05:07 +0530

    feat:multer and cloudinary for file handling

--- node-backend/src/config/cloudinaryConfig.js
+++ node-backend/src/config/cloudinaryConfig.js
@@ -1,13 +1,35 @@
-import { v2 as cloudinary } from 'cloudinary';
+import {v2 as cloudinary} from "cloudinary"
 import dotenv from 'dotenv';
+import fs from "fs"
 
-// Load environment variables
 dotenv.config();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+/* --------------------------------cloudinary configuration -------------------------------------*/
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
-export default cloudinary;
+/* --------------------------------cloudinary utility function -------------------------------------*/
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
