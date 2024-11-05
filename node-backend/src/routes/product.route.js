c35621b8deb0d50ceacf25f2f6b354e4a449e645
Anand Stephan <anandstephan98@gmail.com>
2024-11-05 17:40:29 +0530

    feat:show particular and delete product api

--- node-backend/src/routes/product.route.js
+++ node-backend/src/routes/product.route.js
@@ -13,11 +13,11 @@ import { uploadToCloudinary } from "../middleware/cloudinary.middlewere.js";
 const router = new Router();
 // router.post("/add", multerUpload.array("imageFiles", 10), uploadToCloudinary(), productValidationRules, addProduct)
 // router.put("/edit/:productId", multerUpload.array("imageFiles", 10), uploadToCloudinary(), productValidationRules, updateProduct)
-// router.get("/get/:productId", getProduct)
-// router.delete("/delete/:productId", deleteProduct)
+
 // router.get("/get-all", getAllProducts)
 
 router.post("/product", addProduct);
 router.get("/product", getAllProducts);
-
+router.delete("/product/:productId", deleteProduct);
+router.get("/product/:productId", getProduct);
 export default router;
